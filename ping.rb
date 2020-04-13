543098a4633ed587d0ad10fa7cea88d471b833dd
renen watermeyer <renen@121.co.za>
2020-04-13 16:47:53 +0200

    Add the actual code

---
+++ ping.rb
@@ -0,0 +1,22 @@
+require 'json'
+
+target  = ARGV[0]
+COUNT   = 10
+
+if target.nil? || target==''
+  p 'A target server is required'
+else
+  results = `ping -c#{COUNT} -q #{target}`
+  details = results.split(/\n/)
+
+  packet_loss_percentage = details.last(2)[0][/(\d{1,3})\%\spacket/,1]
+  rtt = details.last.split(' ')
+  h = {
+    target:                 target,
+    pings:                  COUNT,
+    packet_loss_percentage: details.last(2)[0][/(\d{1,3})\%\spacket/,1],
+    rtt:                    rtt[1].split('/').zip(rtt[3].split('/')).to_h,
+  }
+
+  puts h.to_json
+end

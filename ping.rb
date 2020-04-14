8ce969214b984e97a33d7bbd6d0c12607af495b0
renen watermeyer <renen@121.co.za>
2020-04-14 10:44:07 +0200

    Use the COUNT constant

--- ping.rb
+++ ping.rb
@@ -6,7 +6,7 @@ COUNT   = 10
 if target.nil? || target==''
   p 'A target server is required'
 else
-  results = `ping -c2 -q #{target}`
+  results = `ping -c#{COUNT} -q #{target}`
   details = results.split(/\n/)
 
   packet_loss_percentage = details.last(2)[0][/(\d{1,3})\%\spacket/,1]
